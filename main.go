c074c7d38eec6077a47f026e5338afd3b4451c39
Bytedance <mabinshan@bytedance.com>
2023-10-11 19:42:52 +0800

    feat: Add init file

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+const (
+	baiduStopwords = "./baidu_stopwords.txt"
+	cnStopwords    = "./cn_stopwords.txt"
+	hitStopwords   = "./hit_stopwords.txt"
+	scuStopwords   = "./scu_stopwords.txt"
+)
+
+var BaiduStopwords map[string]bool
+var CNStopwords map[string]bool
+var HITStopwords map[string]bool
+var SCUStopwords map[string]bool
+
+func readLines(filename string) map[string]bool {
+	file, err := os.Open(filename)
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+	sc := bufio.NewScanner(file)
+
+	res := make(map[string]bool)
+	for sc.Scan() {
+		res[sc.Text()] = true
+	}
+	return res
+}
+
+func init() {
+	BaiduStopwords = readLines(baiduStopwords)
+	CNStopwords = readLines(cnStopwords)
+	HITStopwords = readLines(hitStopwords)
+	SCUStopwords = readLines(scuStopwords)
+}
---
+++ main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_init(t *testing.T) {
+	allStopwords := map[string]map[string]bool{
+		"baidu": BaiduStopwords,
+		"cn":    CNStopwords,
+		"hit":   HITStopwords,
+		"scu":   SCUStopwords,
+	}
+	for name, stw := range allStopwords {
+		if len(stw) == 0 {
+			t.Errorf("stopwords is empty: %s", name)
+		}
+	}
+}
